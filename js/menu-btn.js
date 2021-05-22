bf97ee646cb9d7a46154ed900e29db1ed58a1898
Damian Wiśniowski <dkontow@gmail.com>
2021-05-22 14:48:56 +0200

    click protection

--- js/menu-btn.js
+++ js/menu-btn.js
@@ -1,3 +1,11 @@
 document.querySelector('.add-flashcards').addEventListener('click', () => {
-    document.querySelector('.add-menu').classList.toggle('show-menu');
+    if (!document.querySelector('.flashcards-list').classList.contains('show-list')) {
+        document.querySelector('.add-menu').classList.toggle('show-menu');
+    }
+
+})
+document.querySelector('.flashcards-list-btn').addEventListener('click', () => {
+    if (!document.querySelector('.add-menu').classList.contains('show-menu')) {
+        document.querySelector('.flashcards-list').classList.toggle('show-list');
+    }
 })
