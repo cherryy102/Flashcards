6748a8a0fe23ce5799f3a50eeb6e096131bb50f2
Damian Wiśniowski <dkontow@gmail.com>
2021-05-25 11:47:53 +0200

    working view flashcards on list

--- js/flashcards.js
+++ js/flashcards.js
@@ -9,21 +9,26 @@ if (JSON.parse(localStorage.getItem('saveWords'))) {
     translations = JSON.parse(localStorage.getItem('saveTranslations'));
     groups = JSON.parse(localStorage.getItem('saveGroups'));
 }
+//showing groups at the beginning
+if (localStorage.getItem(`flashcardsListHtml`)) {
+    document.querySelector('.flashcards-list').innerHTML = localStorage.getItem(`flashcardsListHtml`);
+}
+let inputWord = document.querySelector('.add-menu__input-word');
+let inputTranslation = document.querySelector('.add-menu__input-translate');
+let inputGroup = document.querySelector('.add-menu__input-group');
 const addWords = function() {
-    let inputWordValue = document.querySelector('.add-menu__input-word').value;
-    let inputTranslationValue = document.querySelector('.add-menu__input-translate').value;
-    let inputGroupValue = document.querySelector('.add-menu__input-group').value;
     //if the groups do not exist add to array
-    if (!groups.includes(inputGroupValue) && (inputWordValue !== '' && inputTranslationValue !== '' && inputGroupValue !== '')) {
-        groups.push(inputGroupValue);
+    if (!groups.includes(inputGroup.value) && (inputWord.value !== '' && inputTranslation.value !== '' && inputGroup.value !== '')) {
+        groups.push(inputGroup.value);
         words.push([]);
         translations.push([]);
+        addGroupToList();
     }
     //add content to array
-    if (inputWordValue !== '' && inputTranslationValue !== '' && inputGroupValue !== '') {
-        const groupIndex = groups.indexOf(inputGroupValue);
-        words[groupIndex].push(inputWordValue);
-        translations[groupIndex].push(inputTranslationValue);
+    if (inputWord.value !== '' && inputTranslation.value !== '' && inputGroup.value !== '') {
+        const groupIndex = groups.indexOf(inputGroup.value);
+        words[groupIndex].push(inputWord.value);
+        translations[groupIndex].push(inputTranslation.value);
         //clear value
         document.querySelector('.add-menu__input-word').value = '';
         document.querySelector('.add-menu__input-translate').value = '';
@@ -33,11 +38,31 @@ const addWords = function() {
         localStorage.setItem('saveGroups', JSON.stringify(groups));
     }
 
-
-
 }
 document.querySelector('.add-btn').addEventListener('click', addWords);
-
-// ## correct and incorrect words ###
+// ##show flashcards in list##
+//add group to list
+const addGroupToList = () => {
+        const flashcardsList = document.querySelector('.flashcards-list');
+        // const flashcardsGroup = document.querySelectorAll('.flashcards-list__group');
+        const index = groups.indexOf(inputGroup.value);
+        if (groups.length > 0) {
+            const lastItemIndex = groups.length - 1;
+            const div = document.createElement('div');
+            div.classList.add('flashcards-list__group');
+            div.dataset.item = index;
+            div.innerHTML = `
+        <h1 data-nameGroup=''>${groups[lastItemIndex]}</h1>
+        <div class="flashcards-list__item">
+            <div class="flashcards-list__all-words" data-all='${index}'>All words</div>
+            <div class="flashcards-list__correct" data-correct='${index}'>Correct words</div>
+            <div class="flashcards-list__incorrect" data-incorect='${index}'>Incorrect words</div>
+        </div>`;
+            flashcardsList.appendChild(div);
+        }
+        const flashcardsListHtml = flashcardsList.innerHTML;
+        localStorage.setItem(`flashcardsListHtml`, flashcardsListHtml);
+    }
+    // ## correct and incorrect words ###
 let correct = [];
 let incorrect = [];
